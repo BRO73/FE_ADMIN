efd9531dd8428986861a89733eb0c08807c7747c
Nguyen Huu Minh Tuan <nguyenhuuminhtuan20111@gmail.com>
2025-11-06 16:46:24 +0700

    Resolve merge conflict in api.js

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,6 @@ import { Code } from "lucide-react";
 
 const api = axios.create({
     baseURL: "http://localhost:8082/api",
-    baseURL: "http://localhost:8082/api",
     headers: {
         "Content-Type": "application/json",
     },
